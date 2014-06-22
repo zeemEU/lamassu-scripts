1bd742539adc175bf7a8f678500cc7652c718b9c
Josh Harvey <josh@lamassu.is>
2014-06-21 20:10:26 -0400

    migrations are working

--- migrations/001-add-bills.js
+++ migrations/001-add-bills.js
@@ -1,32 +1,15 @@
 'use strict';
 
-var pg = require('pg');
-var conString = process.env.DATABASE_URL;
-
-exports.up = function(next){
-  pg.connect(conString, function (err, db, done) {
-    if (err) throw err;
-    db.query('CREATE TABLE bills ( ' +
-      'id serial PRIMARY KEY, ' +
-      'device_fingerprint text NOT NULL, ' +
-      'denomination integer NOT NULL, ' +
-      'currency_code text NOT NULL, ' +
-      'transaction_id uuid NOT NULL, ' +
-      'created timestamp NOT NULL DEFAULT now() )', function (err) {
-        done();
-        if (err) throw err;
-        next();
-    });
-  });
+exports.up = function(db, next) {
+  db.query('CREATE TABLE bills ( ' +
+    'id serial PRIMARY KEY, ' +
+    'device_fingerprint text NOT NULL, ' +
+    'denomination integer NOT NULL, ' +
+    'currency_code text NOT NULL, ' +
+    'transaction_id uuid NOT NULL, ' +
+    'created timestamp NOT NULL DEFAULT now() )', next);
 };
 
-exports.down = function(next){
-  pg.connect(conString, function (err, db, done) {
-    if (err) throw err;
-    db.query('DROP TABLE bills', function (err) {
-      done();
-      if (err) throw err;
-      next();
-    });
-  });
+exports.down = function(db, next){
+  db.query('DROP TABLE bills', next);
 };
--- migrations/002-add-bill-validator-events.js
+++ migrations/002-add-bill-validator-events.js
@@ -1,30 +1,13 @@
 'use strict';
 
-var pg = require('pg');
-var conString = process.env.DATABASE_URL;
-
-exports.up = function(next){
-  pg.connect(conString, function (err, db, done) {
-    if (err) throw err;
-    db.query('CREATE TABLE bill_validator_events ( ' +
-      'id serial PRIMARY KEY, ' +
-      'event_type text NOT NULL, ' +
-      'note text, ' +
-      'created timestamp NOT NULL DEFAULT now() )', function (err) {
-        done();
-        if (err) throw err;
-        next();
-    });
-  });
+exports.up = function(db, next) {
+  db.query('CREATE TABLE bill_validator_events ( ' +
+    'id serial PRIMARY KEY, ' +
+    'event_type text NOT NULL, ' +
+    'note text, ' +
+    'created timestamp NOT NULL DEFAULT now() )', next);
 };
 
-exports.down = function(next){
-  pg.connect(conString, function (err, db, done) {
-    if (err) throw err;
-    db.query('DROP TABLE bill_validator_events', function (err) {
-      done();
-      if (err) throw err;
-      next();
-    });
-  });
+exports.down = function(db, next) {
+  db.query('DROP TABLE bills', next);
 };

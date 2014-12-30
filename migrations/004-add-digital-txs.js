df368248ed6f68d53451298bd447d0fbb75cda9d
Josh Harvey <josh@lamassu.is>
2014-12-30 17:51:06 -0500

    add digital-txs migration

--- migrations/004-add-digital-txs.js
+++ migrations/004-add-digital-txs.js
@@ -4,7 +4,12 @@ exports.up = function(db, next) {
   db.query('CREATE TABLE digital_transactions ( ' +
     'id serial PRIMARY KEY, ' +
     'transaction_id integer references transactions(id), ' +
+    'status text, ' +
+    'incoming boolean, ' +
+    'satoshis integer, ' +
     'tx_hash text NULL, ' +
     'error text NULL, ' +
-    'created timestamp NOT NULL DEFAULT now() )', next);
+    'created timestamp NOT NULL DEFAULT now(), ' +
+    'CONSTRAINT digital_transactions_status_txid UNIQUE(status, transaction_id) ' +
+  ')', next);
 };
